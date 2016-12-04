0a257b3dc6594a2585d665e30bd1ed2d46f34d66
avnibarman <avni.barman@gmail.com>
2016-12-04 12:52:11 -0800

    updated

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var DB_PASSWORD = '6163957274';
 var sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: 'mysql',
   host: 'vps.uscitp.com'
-  // _socket :'/var/lib/mysql/mysql.sock'
 
 });
 
@@ -32,8 +31,6 @@ app.use(cors());
 app.use(bodyParser());
 
 app.post('/api/favorites', function (request, response) {
-
-  //response.json(request.body);
 
   var favorite = Favorite.build({
     item: request.body.item
@@ -57,20 +54,7 @@ app.get('/api/favorites', function (request, response) {
 })
 
 app.delete('/api/favorites', function (request, response) {
-  // var promise = Favorite.findAll();
-  // promise.then(function(favorites){
-  //   response.json({
-  //     data:favorites
-  //   })
-  // }
 
-  // Favorite.findById(request.params.id).then(function(item){
-  //
-  //     item.destroy().then(function(item){
-  //       response.json(item);
-  //     });
-  //
-  //   });
   Favorite.destroy({where: {}}).then(function(item){
          response.json(item);
        });
